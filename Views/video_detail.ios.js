2aa2172a2852bd82c31893f52235060aae63bb47
Jose Vildosola <josev55@gmail.com>
2015-09-02 18:05:46 -0300

    Video Detail Skeleton

---
+++ Views/video_detail.ios.js
@@ -0,0 +1,19 @@
+var React = require('react-native');
+
+var {
+	Component,
+	View,
+	Image,
+	WebView
+} = React;
+
+class VideoDetail extends Component {
+	render() {
+		return(
+			<View style={{backgroundColor: '#FAB'}}>
+			</View>
+		);
+	}
+}
+
+module.exports = VideoDetail;
